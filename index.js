9c9552ccf915c17f7ac749e8d51864a48dd3340a
Gurqu1 <gurpreetgs19881@gmail.com>
2025-05-11 13:46:10 -0700

    integrated supabase(postgres database) with SELECT, INSERT operations working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import OpenAI from "openai";
 import dotenv from 'dotenv';
+import supabase from './supabaseClient.js';
 dotenv.config();
 
 
@@ -29,7 +30,7 @@ const openai = new OpenAI({
 const prompt = `
 Analyze the following job posting data and return a JSON object with:
 - company_name
-- Job_title
+- job_title
 - job_description
 - job_location
 - salary_range
@@ -41,7 +42,7 @@ async function generateResponse(data) {
   const response = await openai.chat.completions.create({
     model: "gpt-4.1-nano",
     messages: [
-      { role: "system", content: "You are a backend API that only responds with valid JSON objects and only give answers with certainty do not provide or justify unkown answers." },
+      { role: "system", content: "You are a backend API that only responds with valid JSON objects and only give answers with certainty do not provide or justify unkown answers and if there is an answer you dont know they just say unknown." },
       { role: "user", content: `${prompt}\n\nData:\n${JSON.stringify(data)}` }
     ],
     temperature: 0.2
@@ -52,6 +53,7 @@ async function generateResponse(data) {
 
   try {
     const json = JSON.parse(output);
+    insertDataToDatabase(json)
     console.log("Parsed JSON:", json);
   } catch (e) {
     console.warn("Could not parse response as JSON.");
@@ -61,17 +63,41 @@ async function generateResponse(data) {
 
 //Open api end
 
+async function printDatabse() {
+  const {data, error} = await supabase.from('Jobs').select('*');
+  if(error){
+    console.error("Error fetching data:", error);
+  }
+  else{
+    console.log("Data from database:", data);
+  }
+}
+
+async function insertDataToDatabase(data) {
+  try{
+    await supabase.from('Jobs').insert([data]);
+  }
+  catch (error) {
+    console.error("Error inserting data:", error);
+  }
+}
+
+//Routes Start
+
 
 app.post('/url', (req, res) => {
   const bodyData = req.body;
   console.log(bodyData)
   res.send('Hello World url!');
   generateResponse(bodyData);
+  printDatabse();
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+//Routes End
 
 // Start the server
 app.listen(port, () => {
